f54bcec1357272fd97db44f319216da24c99c66a
Danny <deleon.danielo@gmail.com>
2020-01-15 19:15:16 -0300

    Fin de la sección 16

---
+++ src/app/pages/hospitales/hospitales.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { Hospital } from '../../models/hospital.model';
+import { HospitalService } from '../../services/service.index';
+import { ModalUploadService } from '../../components/modal-upload/modal-upload.service';
+
+import Swal from 'sweetalert2';
+
+
+@Component({
+  selector: 'app-hospitales',
+  templateUrl: './hospitales.component.html',
+  styles: []
+})
+export class HospitalesComponent implements OnInit {
+
+  hospitales: Hospital[] = [];
+  
+
+  constructor(
+    public _hospitalService: HospitalService,
+    public _modalUploadService: ModalUploadService
+  ) { }
+
+  ngOnInit() {
+    this.cargarHospitales();
+    this._modalUploadService.notificacion
+          .subscribe( () => this.cargarHospitales());
+  }
+
+  buscarHospital( termino: string ){
+
+    if ( termino.length <= 0){
+      this.cargarHospitales();
+      return;
+    }
+
+    this._hospitalService.buscarHospital(termino)
+          .subscribe( hospitales => this.hospitales = hospitales );
+
+  }
+
+  cargarHospitales() {
+    this._hospitalService.cargarHospitales()
+          .subscribe( hospitales => this.hospitales = hospitales );
+  }
+
+  guardarHospital( hospital: Hospital ) {
+    this._hospitalService.actualizarHospital( hospital )
+          .subscribe();
+  }
+
+  borrarHospital( hospital: Hospital ) {
+    this._hospitalService.borrarHospital( hospital._id )
+          .subscribe( () => this.cargarHospitales());
+  }
+
+  async crearHospital() {
+    const { value: nombreHospital } = await Swal.fire({
+      title: 'Crear Hospital',
+      text: 'Ingrese el nombre del hospital',
+      input: 'text',
+      icon: 'info'
+    });
+    
+    if (!nombreHospital || nombreHospital.length ===0 ){
+      return;
+    }
+    this._hospitalService.crearHospital( nombreHospital )
+          .subscribe( () => this.cargarHospitales());
+  }
+
+  actualizarImagen( hospital: Hospital) {
+    this._modalUploadService.mostrarModal( 'hospitales', hospital._id);
+  }
+
+
+}
